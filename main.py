d56077b0fabbc4a4d4034f86ef9803f20b3aaa25
pauloeduardo145 <pauloeduardodasilvaporto613@gmail.com>
2026-05-14 19:09:13 -0300

    Consertado o erro que causava o None nos resultados do historico e iniciando aqui a interface

--- main.py
+++ main.py
@@ -2,7 +2,23 @@ from operacoes import somar, subtrair, multiplicacao, divisao
 from arquivos import ler_historico, salvar_arquivo
 
 historico = ler_historico()
+
 while True:
+
+    def adicionar_historico(numero1, numero2, operacao, resultado):
+            expressao = f"{numero1} {operacao} {numero2} = {resultado}"
+            historico.append(expressao)
+            salvar_arquivo(historico)
+
+    def listar_historico(historico):
+        if historico:
+            for i, tarefa in enumerate(historico, 1):
+                print(f"{i}. {tarefa}")
+        else:
+                print("Não possui historico")
+
+
+
     print("Calculadora em PYTHON")
     print("Operações utilizaveis: +, -, *, /")
 
@@ -14,35 +30,40 @@ while True:
     opcao = input("Escolha...")
 
     if opcao == "1":
+
+        resultado = None
+
         numero1 = float(input("Digite o primeiro numero: "))
         operacao = input("Digite a operação: ")
         numero2 = float(input("Digite o segundo numero: "))
 
         if operacao == "+":
-            print("Resultado:", somar(numero1, numero2))
+            resultado = somar(numero1, numero2)
+            print("Resultado:", resultado)
+            salvar_arquivo(historico)
         elif operacao == "-":
-            print("Resultado:", subtrair(numero1, numero2))
+            resultado = subtrair(numero1, numero2)
+            print("Resultado:", resultado)
+            salvar_arquivo(historico)
         elif operacao == "*":
-            print("Resultado:", multiplicacao(numero1, numero2))
+            resultado = multiplicacao(numero1, numero2)
+            print("Resultado:", resultado)
+            salvar_arquivo(historico)            
         elif operacao == "/":
-            print("Resultado:", divisao(numero1, numero2))
+            resultado = divisao(numero1, numero2)
+            print("Resultado:", resultado)
+            salvar_arquivo(historico)
         else:
             print("Operação invalida!")
-        resultado = None
+        
+        adicionar_historico(numero1, numero2, operacao, resultado)
+        listar_historico(historico)
+
+
 
     elif opcao == "2":
+        print("Vai salvar...", historico)
         salvar_arquivo(historico)
         break
     
 
-def adicionar_historico():
-        expressao = f"{numero1} {operacao} {numero2} = {resultado}"
-        historico.append(expressao)
-        salvar_arquivo(historico)
-
-def listar_historico():
-    if historico:
-        for i, tarefa in enumerate(historico, 1):
-            print(f"{i}. {tarefa}")
-    else:
-            print("Não possui historico")
