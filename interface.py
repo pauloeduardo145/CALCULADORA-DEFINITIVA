1d68ed5c54f11db48f164d2825124473ba12ac25
pauloeduardo145 <pauloeduardodasilvaporto613@gmail.com>
2026-05-18 16:52:41 -0300

    ATUALIZAÇÃO, interface começando a ser trabalhada!

--- interface.py
+++ interface.py
@@ -1,13 +1,25 @@
 import tkinter as tk
+from operacoes import somar, subtrair, multiplicacao, divisao
 
 janela = tk.Tk()
 
-janela.title("Interface")
+janela.title("Calculadora Definitiva")
 janela.geometry("300x200")
 
-Entry()
-
-label = tk.Label(janela, text="Olá, Mundo")
+label = tk.Label(janela, text="Digite um valor")
 label.pack()
+
+label1 = tk.Label(janela, text="a")
+label1.pack()
+
+def set_texto(valor):
+    label1.config(text=valor)
+
+botao1 = tk.Button(janela, text="1", command=lambda: set_texto("1"))
+botao1.pack(pady=5)
+
+
+botao2 = tk.Button(janela, text="2", command=lambda: set_texto("2"))
+botao2.pack(pady=5)
 
 janela.mainloop()
